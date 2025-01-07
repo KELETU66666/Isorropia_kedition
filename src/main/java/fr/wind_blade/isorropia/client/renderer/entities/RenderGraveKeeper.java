1214954de04e319f636fdbd749f8bd812fb1c8ec
a <EYCHFSJD@gmail.com>
2025-01-07 08:41:59 +0800

    improve imports

--- src/main/java/fr/wind_blade/isorropia/client/renderer/entities/RenderGraveKeeper.java
+++ src/main/java/fr/wind_blade/isorropia/client/renderer/entities/RenderGraveKeeper.java
@@ -6,7 +6,6 @@ import net.minecraft.client.model.ModelOcelot;
 import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.client.renderer.entity.RenderLiving;
 import net.minecraft.client.renderer.entity.RenderManager;
-import net.minecraft.entity.passive.EntityOcelot;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
